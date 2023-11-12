b87d113bf3a5cc3e8efb20a2bee0a7dc4e20e73b
AnıL <AnL@DESKTOP-THANGOR>
2023-11-13 00:44:38 +0300

    Day4

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        T[] items;
+
+        //bir class newlendiğinde çalışan blok constructor
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            //new dediğimiz zaman yeni referans numarası alır önceki elemanlar silinir. bu yüzden bu elemanlar silinmesin diye temparray e atadık
+
+            items = new T[items.Length + 1];
+            //mevcut eleman sayısını 1 arttırır
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            
+            items[items.Length-1] = item;
+
+        }
+
+
+
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizileri genişletemiyoruz  
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" }; // 4 elemanlı bir array tanımladığımız zaman 5 elemanlı yapamıyoruz
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];   //new dediğimiz için bellekte yeni bir adres oluşturduk (5 elemanlı boş bir array) o yüzden 0,1,2,3 boş 
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+
+}
