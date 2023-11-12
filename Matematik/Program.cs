2d3ee7f02a90d9cc9ca6651ada87476c2128d297
AnıL <AnL@DESKTOP-THANGOR>
2023-11-12 22:04:14 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FourOperations fourOperations = new FourOperations();
+            fourOperations.Total(5, 6);
+            fourOperations.Total(7, 8);
+        }
+    }
+}
